dc72fcef084d19a02bcda9e1891c6ec74883f8d2
Cem EKE <56502922+cmkqwerty@users.noreply.github.com>
2020-08-14 15:41:02 +0300

    Add Tagging a User Bot

---
+++ user_tagging_bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.on('message', (receivedMessage) => {
+    // Prevent from responding to its own messages
+    if (receivedMessage.author == client.user) {
+        return
+    }
+
+    receivedMessage.channel.send("Message received from " + receivedMessage.author.toString() + ": " + receivedMessage.content)
+})
+
+client.login("XXXXXXXXXXX") // Replace XXXXX with your bot token
